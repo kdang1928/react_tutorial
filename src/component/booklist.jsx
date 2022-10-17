796bc47801475dce1ffbe53355443776d06f520d
kdang1928 <kimdang@ucsb.edu>
2022-10-17 13:15:50 -0700

    Adding the key prop in the list

--- src/component/booklist.jsx
+++ src/component/booklist.jsx
@@ -5,16 +5,19 @@ import '../index.css'
 
 const books = [
     {
+        id: 1,
         title:'Harry Potter Hogwarts Library', 
         author: 'J.K. Rowling', 
         image: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/41UjBn+cYtL._SX258_BO1,204,203,200_.jpg'
     },
     {
+        id: 2,
         title:'Percy Jackson and the Olympians Box Set', 
         author: 'Rick Riordan', 
         image: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/41d3Xl4fSFS._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
     },
     {
+        id: 3,
         title:'The Alchemist', 
         author: 'Paul Coelho', 
         image: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51kcX5PpaZL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
@@ -25,8 +28,7 @@ export default function Booklist() {
     return (
         <section className="booklist">
            {books.map((book) => {
-            const {title, author, image} = book;
-            return <Book title={title} author={author} image={image}/>
+            return <Book key={book.id} book={book}/>
            })}
         </section>
     )
@@ -34,11 +36,12 @@ export default function Booklist() {
 
 const Book = (props) => {
     console.log(props)
+    const {title, author, image} = props.book;
     return <article className="book">
-        <img src={props.image} alt="Harry Potter Complete Box Set"/>
+        <img src={image} alt="Harry Potter Complete Box Set"/>
         {/* Inline styling: camelCase, JS Object, overrides all other stylings */}
-        <h3 style={{color:'#617d98', fontSize:'16pt', marginTop:'0.25rem'}}>{props.title}</h3>
-        <p>{props.author}</p>
+        <h3 style={{color:'#617d98', fontSize:'16pt', marginTop:'0.25rem'}}>{title}</h3>
+        <p>{author}</p>
     </article>
 }
 
