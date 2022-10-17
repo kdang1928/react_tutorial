4385407732e2ba538d699488588ead1b2c3d149e
kdang1928 <kimdang@ucsb.edu>
2022-10-17 11:14:08 -0700

    ArrowNotation can also be used for creating components

--- src/component/greeting.jsx
+++ src/component/greeting.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+// import ReactDom from 'react-dom'
 
 export default function Greeting() {
     return (
         <h1>Welcome to the React Tutorial!</h1>
     )
 }
+
+// const ArrowNotationComponent = () => {
+//     return <h1>Hello World</h1>
+// }
+
+// ReactDom.render(<ArrowNotationComponent/>, document.getElementById('root'))
