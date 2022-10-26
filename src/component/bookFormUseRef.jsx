0686e0be81c8ad95fd4a162171473c1c262196f8
kdang1928 <kimdang@ucsb.edu>
2022-10-26 14:04:46 -0700

    Minor changes

--- src/component/bookFormUseRef.jsx
+++ src/component/bookFormUseRef.jsx
@@ -3,17 +3,22 @@ import { BookContext } from "../App";
 
 export default function BookFormUseRef() {
   const [booklist, setBookList] = useContext(BookContext);
-  const formData = useRef({
-    title: "Howl's Moving Castle",
-    author: "Diana Wynne Jones",
-    image:
-      "https://www.foliosociety.com/media/catalog/product/m/v/mvc_book_1.png?quality=80&fit=bounds&height=700&width=700&canvas=700:700",
-  });
+  const formTitle = useRef("Howl's Moving Castle");
+  const formAuthor = useRef("Diana Wynne Jones");
+  const formImage = useRef(
+    "https://www.foliosociety.com/media/catalog/product/m/v/mvc_book_1.png?quality=80&fit=bounds&height=700&width=700&canvas=700:700"
+  );
 
   const addBook = (e) => {
     e.preventDefault();
-    console.log(formData.current);
-    setBookList([...booklist, formData.current]);
+
+    const data = {
+      title: formTitle.current?.value,
+      author: formAuthor.current?.value,
+      image: formImage.current?.value,
+    };
+
+    setBookList([...booklist, data]);
   };
 
   return (
@@ -23,23 +28,11 @@ export default function BookFormUseRef() {
         {" "}
         {/* alt: can add onSubmit and change button type to submit */}
         <label>Title:</label>
-        <input
-          placeholder="Title"
-          ref={formData.title}
-          value={formData.current.title}
-        />
+        <input placeholder="Title" ref={formTitle} />
         <label>Author:</label>
-        <input
-          placeholder="Author"
-          ref={formData.author}
-          value={formData.current.author}
-        />
+        <input placeholder="Author" ref={formAuthor} />
         <label>Image URL:</label>
-        <input
-          placeholder="Image URL"
-          ref={formData.image}
-          value={formData.current.image}
-        />
+        <input placeholder="Image URL" ref={formImage} />
         <button
           onClick={(e) => {
             addBook(e);
